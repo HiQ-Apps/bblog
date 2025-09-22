bc11e3c46825eb315a0061f1f6a01dc246efdba8
Daywick <derekli11204@gmail.com>
2025-09-22 01:38:01 -0400

    Potential fix for missing navlink

--- bblog/src/components/composite/mobileNavMenu.tsx
+++ bblog/src/components/composite/mobileNavMenu.tsx
@@ -2,32 +2,35 @@ import {
   NavigationMenu,
   NavigationMenuItem,
   NavigationMenuLink,
+  NavigationMenuList,
 } from "@/components/ui/navigation-menu";
 import Link from "next/link";
 
 const MobileNavMenu = () => {
   return (
-    <NavigationMenu className="w-full list-none space-x-4 bg-primary border-b border-sidebar-border font-playfair">
-      <NavigationMenuItem>
-        <NavigationMenuLink asChild>
-          <Link href="/">Home</Link>
-        </NavigationMenuLink>
-      </NavigationMenuItem>
-      <NavigationMenuItem>
-        <NavigationMenuLink asChild>
-          <Link href="/blog">Blog</Link>
-        </NavigationMenuLink>
-      </NavigationMenuItem>
-      <NavigationMenuItem>
-        <NavigationMenuLink asChild>
-          <Link href="/disclaimer">Disclaimer</Link>
-        </NavigationMenuLink>
-      </NavigationMenuItem>
-      <NavigationMenuItem>
-        <NavigationMenuLink asChild>
-          <Link href="/about">About</Link>
-        </NavigationMenuLink>
-      </NavigationMenuItem>
+    <NavigationMenu className="w-full bg-primary border-b border-sidebar-border font-playfair justify-center">
+      <NavigationMenuList className="mx-auto flex gap-6 py-2">
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <Link href="/">Home</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <Link href="/blog">Blog</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <Link href="/about">About</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <Link href="/disclaimer">Disclaimer</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
     </NavigationMenu>
   );
 };
