d0d419cbcd7a2d2253f34bf723fb63ddeb77dcc9
Daywick <derekli11204@gmail.com>
2025-10-04 16:30:11 -0400

    export default

--- bblog/src/app/api/posts/tags/route.ts
+++ bblog/src/app/api/posts/tags/route.ts
@@ -17,7 +17,7 @@ function parseTags(url: URL): string[] {
   );
 }
 
-export async function GET(req: Request) {
+export default async function GET(req: Request) {
   const url = new URL(req.url);
   const tags = parseTags(url);
   if (tags.length === 0) {
