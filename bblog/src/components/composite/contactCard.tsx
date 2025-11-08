1237b0c89d18a42b503ecef1831d522020d752bc
Daywick <derekli11204@gmail.com>
2025-11-08 13:25:58 -0500

    etsy route added

--- bblog/src/components/composite/contactCard.tsx
+++ bblog/src/components/composite/contactCard.tsx
@@ -1,23 +1,30 @@
 import Image from "next/image";
 import { EnvelopeClosedIcon } from "@radix-ui/react-icons";
+import { FaEtsy } from "react-icons/fa6";
 
 const ContactCard = () => {
   return (
-    <div className="bg-primary/5 text-popover-foreground px-4 z-20">
-      <div className="inline-flex items-center justify-center gap-2 p-3">
+    <div className="bg-primary text-popover-foreground px-4 z-20">
+      <div
+        className="
+          mx-auto
+          flex flex-row items-center justify-center
+          xl:grid xl:grid-cols-2 xl:gap-3
+        "
+      >
         <a
           href="https://www.instagram.com/thegoodstandard"
           target="_blank"
           rel="noopener"
           aria-label="Instagram"
-          className="shrink-0 hover:bg-black/20 p-3 rounded-full"
+          className="shrink-0 hover:bg-secondary p-3 rounded-full flex items-center justify-center"
         >
           <Image
             src="/instagram.svg"
             alt="Instagram Icon"
             width={24}
             height={24}
-            className="shrink-0 "
+            className="shrink-0"
           />
         </a>
         <a
@@ -25,7 +32,7 @@ const ContactCard = () => {
           target="_blank"
           rel="noopener"
           aria-label="Pinterest"
-          className="shrink-0 hover:bg-black/20 p-3 rounded-full"
+          className="shrink-0 hover:bg-secondary p-3 rounded-full flex items-center justify-center"
         >
           <Image
             src="/pinterest.svg"
@@ -38,9 +45,18 @@ const ContactCard = () => {
         <a
           href="mailto:contact@thegoodstandard.org"
           aria-label="Email"
-          className="shrink-0 hover:bg-black/20 p-3 rounded-full"
+          className="shrink-0 hover:bg-secondary p-3 rounded-full flex items-center justify-center"
         >
           <EnvelopeClosedIcon className="h-6 w-6" />
+        </a>
+        <a
+          href="https://www.etsy.com/shop/TheGoodStandard"
+          target="_blank"
+          rel="noopener"
+          aria-label="Etsy"
+          className="shrink-0 hover:bg-secondary p-3 h-12 w-12 rounded-full flex items-center justify-center"
+        >
+          <FaEtsy className="h-5 w-5" />
         </a>
       </div>
     </div>
